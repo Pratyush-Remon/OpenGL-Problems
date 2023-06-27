e02ba4536b70f412b9b2515ea0067bab8ba63f52
Pratyush-Remon <pratyushsoni2004@gmail.com>
2023-06-27 10:46:24 +0530

    various headers added to include new problem added

--- Problem1.cpp
+++ Problem1.cpp
@@ -1,14 +1,18 @@
 #include <glew.h>
 #include <glut.h>
 #include <iostream>
+#include <ogldev_math_3d.h>
+/*This code creates a window and renders different colors*/
 
-GLclampf randomizer(GLclampf c) {
+
+GLclampf randomizer(GLclampf c) {				//This function helps in not really randomize but change the value passed
 	c += 1.0f / 256.0f;
 	if (c >= 1) {
 		c = 0.0f;
 	}
 	return c;
 }
+
 
 void RenderSceneCB() {
 	static GLfloat Red = 0.2f, Green = 0.3f, Blue = 0.4f, Alpha = 1.0f;
@@ -19,19 +23,19 @@ void RenderSceneCB() {
 
 	printf("%f\n%f\n%f\n%f\n", Red, Green, Blue, Alpha);
 
-	glClearColor(Red, Green, Blue, Alpha);
+	glClearColor(Red, Green, Blue, Alpha); // this function tell glclear the RGBA values
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	glutSwapBuffers();
-	glutPostRedisplay();
+	glutSwapBuffers();  // to swap the front and back buffers
+	glutPostRedisplay();// it creates a flag so that RenderScene keeps updating
 }
 
 
 
 
 int main(int argc, char* argv[]) {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInit(&argc, argv);  // initiating glut
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA); 
 
 	int height = 1980;
 	int width = 1080;
